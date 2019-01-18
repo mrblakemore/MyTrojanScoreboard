429f69987d1f5adc9a16d170b67fd42febf6708e
crblakemore <crblakemore@gmail.com>
2019-01-17 20:20:29 -0500

    Initial Commit

---
+++ MyTrojanScoreboard/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyTrojanScoreboard
+//
+//  Created by Robby Blakemore on 1/17/19.
+//  Copyright © 2019 Robby Blakemore. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
